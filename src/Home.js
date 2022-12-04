4dbd303f543844bba4af62f96d533d6e97a1d119
unknown <chimaobiu@yahoo.com>
2022-12-04 08:17:06 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='chima'>
+        <h1>CHIMA REACT CALCULATOR</h1>
+      
+    </div>
+  )
+}
+
+export default Home;
